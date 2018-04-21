48d152e2071f5e63b7a4d40ba9ac23d7f0aa51a6
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2018-04-21 17:27:37 -0400

    initial styleguide

---
+++ src/assignments/project-2/styleguide/README.md
@@ -0,0 +1,6 @@
+# styleguide
+
+This folder contains the styleguide components to be used in building the app.
+
+The intent is to showcase styled components without logic, ready to be
+used by the front end.
---
+++ src/assignments/project-2/styleguide/samples/Palette.jsx.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Palette extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <section className="Palette styleguide">
+        <h3>Palette Colors</h3>
+        <ul>
+          <li className="palette--color primary-color">$primary-color: #EFFFE8;</li>
+          <li className="palette--color primary-text-color">$primary-text-color: #110800;</li>
+          <li className="palette--color secondary-color">$secondary-color: #efefef;</li>
+          <li className="palette--color secondary-text-color">$secondary-text-color: #235789;</li>
+          <li className="palette--color highlight-color">$highlight-color: white;</li>
+          <li className="palette--color error-color">$error-color: #C1292E;</li>
+          <li className="palette--color accent-color-blue">$accent-color-blue: #efefef;</li>
+          <li className="palette--color accent-color-red">$accent-color-red: #C1292E;</li>
+          <li className="palette--color accent-color-yellow">$accent-color-yellow: #F1D302;</li>
+        </ul>
+      </section>
+    )
+  }
+}
