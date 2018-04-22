c929f38f42921c5e813a139d326a99460a1c827e
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2018-04-22 09:08:13 -0400

    hoping this is better for these files

---
+++ src/ui/app.jsx.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Module from '../assignments/project-2/index.jsx.js'
+
+const App = ({auth, ...props}) => {
+	switch (auth.status) {
+		case `init`: return <span>Authorizing</span>
+		case `failure`: return <span>{auth.message}</span>
+
+		default: return <Module {...props} />
+	}
+}
+
+export default App
