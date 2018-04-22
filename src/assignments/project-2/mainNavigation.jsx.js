47a4d1f98fc4fa395b521b0f93658ea6f60b0304
Annette Lin <al261310@gmail.com>
2018-04-22 10:34:33 -0400

    navbar changes and routing

--- src/assignments/project-2/mainNavigation.jsx.js
+++ src/assignments/project-2/mainNavigation.jsx.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class MainNavigation extends React.Component {
-  render() {
-    return 		<nav>
+const mainNavigation = () => (
+  <Router>
+    <nav>
+      <ul>
+      <li>
+          <Link to="/memberlist">Member List</Link>
+        </li>
+        <li>
+          <Link to="/channellist">Channel List</Link>
+        </li>
+      </ul>
+      <Route path="/memberlist" component={memberList} />
+      <Route path="/channellist" component={channelList} />
+    </nav>
+  </Router>
+);
 
-    		</nav>
-  }
-}
+const memberList = () => (
+  <div className = "member-list">
+    <h2>View Member List</h2>
+  </div>
+);
 
-export default MainNavigation
+const channelList = () => (
+	<div className = "channel-list">
+	    <ul>
+	      <li>Pets</li>
+		  <li>Movies</li>
+		  <li>Food</li>
+	      <li>Music</li>
+	    </ul>
+   </div>
+);
+
+
+
+export default mainNavigation
+
